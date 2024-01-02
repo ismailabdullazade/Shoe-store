6b77f4c37a01a83427fe775f7217e8647d36b48d
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-01-02 19:46:34 +0400

    hero and shoeCard

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <main className="relative">
       <Nav/>
-      <section className="xl:padding-1 wide:padding-r padding-b">
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero/>
       </section>
       <section className="padding">
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 // import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
-// import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+import { bigShoe1, bigShoe2, bigShoe3, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
 
 export const navLinks = [
     {href:"#home", label:"Home"},
@@ -12,4 +12,10 @@ export const statistics = [
     {value:"1k+", label:"Brands"},
     {value:"500+", label:"Shops"},
     {value:"250k+", label:"Customers"}
+];
+
+export const shoes = [
+    {thumbnail:thumbnailShoe1, bigShoe:bigShoe1},
+    {thumbnail:thumbnailShoe2, bigShoe:bigShoe2},
+    {thumbnail:thumbnailShoe3, bigShoe:bigShoe3}
 ];
